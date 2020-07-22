52586506f39aa2b87d2e10a427ffc12825c21e13
Himanshu Parihar <himanshuparihar19@gmail.com>
2020-07-21 20:04:00 -0400

    Add files via upload

---
+++ Assignment 2 Python .py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print('Hello World!')
+
+
+# In[21]:
+
+
+#new line error
+print("Hello 
+      python")
+
+
+# In[25]:
+
+
+##back_slash
+print("Hello       python")
+
+
+# In[26]:
+
+
+##triple quote
+print(""" Hello Every One""")
+
+
+# In[30]:
+
+
+## triple quote not getting correct formatting
+print("""
+
+ |\   /|
+  \|_|/
+  /. .\
+ =\_Y_/=
+  {>o<}
+
+""")
+
+
+# In[32]:
+
+
+#string inside the quote
+print('Python\'s world')
+
+
+# In[34]:
+
+
+#Escape sequence of string space
+print ("Hello\tPython")
+
+
+# In[37]:
+
+
+#line seprator
+print("Hello\nPython")
+
+
+# In[15]:
+
+
+name = "XYZ"
+marks = 90.54
+age = 26
+print(f"The name of person is {name} marks are {marks} and age is {age}")
+
+
+# In[19]:
+
+
+_10=6
+if1= 7
+if1>_10
+
+
+# In[20]:
+
+
+#exponent
+print(6**2)
+
