54d67e76fa6a5f0919a013fa36b06bcacf5cb419
Himanshu Parihar <himanshuparihar19@gmail.com>
2020-07-29 23:59:02 -0400

    Add files via upload

---
+++ Python day 6 assignment.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+list1=[1,2,3,4,5,7,8]
+list2=["a","b","c","d","e"]
+combined={list1[i]:list2[i] for i in range(len(list2))} 
+print(combined)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
